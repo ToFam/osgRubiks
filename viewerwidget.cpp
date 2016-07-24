5d8404d36d7678141599cbac34ff65dd8972d953
ToFam <ToFam@gmx.de>
2016-07-25 01:07:43 +0200

    Made a Cube

--- viewerwidget.cpp
+++ viewerwidget.cpp
@@ -61,75 +61,82 @@ void ViewerWidget::paintEvent(QPaintEvent *)
 void ViewerWidget::createScene()
 {
     m_scene = new osg::Group();
-    osg::Geode* pyramidGeode = new osg::Geode();
-    osg::Geometry* pyramidGeometry = new osg::Geometry();
+    osg::Geode* cubeGeode = new osg::Geode();
+    osg::Geometry* cubeGeometry = new osg::Geometry();
 
-    pyramidGeode->addDrawable(pyramidGeometry);
-    m_scene->addChild(pyramidGeode);
+    cubeGeode->addDrawable(cubeGeometry);
+    m_scene->addChild(cubeGeode);
 
-    osg::Vec3Array* pyramidVertices = new osg::Vec3Array;
-    pyramidVertices->push_back( osg::Vec3( 0, 0, 0) ); // front left
-    pyramidVertices->push_back( osg::Vec3(10, 0, 0) ); // front right
-    pyramidVertices->push_back( osg::Vec3(10,10, 0) ); // back right
-    pyramidVertices->push_back( osg::Vec3( 0,10, 0) ); // back left
-    pyramidVertices->push_back( osg::Vec3( 5, 5,10) ); // peak
+    osg::Vec3Array* cubeVertices = new osg::Vec3Array;
+    cubeVertices->push_back( osg::Vec3( 0, 0, 0) );  // 0 lower front left
+    cubeVertices->push_back( osg::Vec3(10, 0, 0) );  // 1 lower front right
+    cubeVertices->push_back( osg::Vec3(10,10, 0) );  // 2 lower back right
+    cubeVertices->push_back( osg::Vec3( 0,10, 0) );  // 3 lower back left
+    cubeVertices->push_back( osg::Vec3( 0, 0, 10) ); // 4 upper front left
+    cubeVertices->push_back( osg::Vec3(10, 0, 10) ); // 5 upper front right
+    cubeVertices->push_back( osg::Vec3(10,10, 10) ); // 6 upper back right
+    cubeVertices->push_back( osg::Vec3( 0,10, 10) ); // 7 upper back left
 
-    pyramidGeometry->setVertexArray( pyramidVertices );
+    cubeGeometry->setVertexArray( cubeVertices );
 
-    osg::DrawElementsUInt* pyramidBase =
+    osg::DrawElementsUInt* cubeBase =
         new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
-    pyramidBase->push_back(3);
-    pyramidBase->push_back(2);
-    pyramidBase->push_back(1);
-    pyramidBase->push_back(0);
-    pyramidGeometry->addPrimitiveSet(pyramidBase);
+    cubeBase->push_back(3);
+    cubeBase->push_back(2);
+    cubeBase->push_back(1);
+    cubeBase->push_back(0);
+    cubeGeometry->addPrimitiveSet(cubeBase);
 
-    osg::DrawElementsUInt* pyramidFaceOne =
-        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
-    pyramidFaceOne->push_back(0);
-    pyramidFaceOne->push_back(1);
-    pyramidFaceOne->push_back(4);
-    pyramidGeometry->addPrimitiveSet(pyramidFaceOne);
+    osg::DrawElementsUInt* cubeFront =
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    cubeFront->push_back(0);
+    cubeFront->push_back(1);
+    cubeFront->push_back(5);
+    cubeFront->push_back(4);
+    cubeGeometry->addPrimitiveSet(cubeFront);
 
-    osg::DrawElementsUInt* pyramidFaceTwo =
-        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
-    pyramidFaceTwo->push_back(1);
-    pyramidFaceTwo->push_back(2);
-    pyramidFaceTwo->push_back(4);
-    pyramidGeometry->addPrimitiveSet(pyramidFaceTwo);
+    osg::DrawElementsUInt* cubeRight =
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    cubeRight->push_back(1);
+    cubeRight->push_back(2);
+    cubeRight->push_back(6);
+    cubeRight->push_back(5);
+    cubeGeometry->addPrimitiveSet(cubeRight);
 
-    osg::DrawElementsUInt* pyramidFaceThree =
-        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
-    pyramidFaceThree->push_back(2);
-    pyramidFaceThree->push_back(3);
-    pyramidFaceThree->push_back(4);
-    pyramidGeometry->addPrimitiveSet(pyramidFaceThree);
+    osg::DrawElementsUInt* cubeLeft =
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    cubeLeft->push_back(3);
+    cubeLeft->push_back(0);
+    cubeLeft->push_back(4);
+    cubeLeft->push_back(7);
+    cubeGeometry->addPrimitiveSet(cubeLeft);
 
-    osg::DrawElementsUInt* pyramidFaceFour =
-        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
-    pyramidFaceFour->push_back(3);
-    pyramidFaceFour->push_back(0);
-    pyramidFaceFour->push_back(4);
-    pyramidGeometry->addPrimitiveSet(pyramidFaceFour);
+    osg::DrawElementsUInt* cubeBack =
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    cubeBack->push_back(2);
+    cubeBack->push_back(3);
+    cubeBack->push_back(7);
+    cubeBack->push_back(6);
+    cubeGeometry->addPrimitiveSet(cubeBack);
+
+    osg::DrawElementsUInt* cubeTop =
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    cubeTop->push_back(6);
+    cubeTop->push_back(7);
+    cubeTop->push_back(4);
+    cubeTop->push_back(5);
+    cubeGeometry->addPrimitiveSet(cubeTop);
 
     osg::Vec4Array* colors = new osg::Vec4Array;
-    colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 0 red
-    colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) ); //index 1 green
-    colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f) ); //index 2 blue
-    colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f) ); //index 3 white
-    colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 4 red
+    colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) );
+    colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) );
+    colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f) );
+    colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f) );
+    colors->push_back(osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f) );
+    colors->push_back(osg::Vec4(0.0f, 1.0f, 1.0f, 1.0f) );
 
-    pyramidGeometry->setColorArray(colors);
-    pyramidGeometry->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
-
-    osg::PositionAttitudeTransform* pyramidTwoXForm =
-        new osg::PositionAttitudeTransform();
-
-    m_scene->addChild(pyramidTwoXForm);
-    pyramidTwoXForm->addChild(pyramidGeode);
-
-    osg::Vec3 pyramidTwoPosition(15,0,0);
-    pyramidTwoXForm->setPosition( pyramidTwoPosition );
+    cubeGeometry->setColorArray(colors);
+    cubeGeometry->setColorBinding(osg::Geometry::BIND_PER_PRIMITIVE_SET);
 
     m_scene->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
 }
